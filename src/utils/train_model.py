3bc2e66b847b6042aa4707263a0fa348839cf3f7
bak siu <atomicw63.546@gmail.com>
2025-12-04 22:06:27 +0900

    Add hyperparameters configuration to model training function

--- src/utils/train_model.py
+++ src/utils/train_model.py
@@ -10,8 +10,9 @@ model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
 dotenv.load_dotenv()
 data_dir = os.getenv("DATA_PATH", "./data")
 yaml_path = os.getenv("DATA_YAML_PATH", f"{data_dir}/data.yaml")
+hyperparameters_path = os.getenv("HYPERPARAMETERS_PATH", None)
 project_dir = os.getenv("PROJECT_PATH", "./runs")
 
 if __name__ == "__main__":
     # Train the model
-    results = model.train(data=yaml_path, epochs=1000, patience=100, imgsz=320, batch=4, project=project_dir)
+    results = model.train(data=yaml_path, cfg=hyperparameters_path, epochs=1000, patience=100, imgsz=320, batch=4, project=project_dir)
